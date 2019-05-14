b108b3d0b1ba1120c4bfb5ae7e0529b33e6a743c
narita <238369+narita1980@users.noreply.github.com>
2019-05-15 06:37:13 +0900

    paypal専用のモジュールがあったので、そっちをinstallするように変更しました

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
 
 class PaypalController extends Controller
 {
+    private $api_context;
+
+    public function __construct() {
+        /** PayPal api context **/
+        $this->api_context = new ApiContext(
+            new OAuthTokenCredential(config('paypal.client_id'), config('paypal.secret'))
+        );
+        $this->api_context->setConfig(config('paypal.settings'));
+    }
+
     public function index(Request $request){
         return response()->json(['apple'=>'red','peach'=>'pink']);
     }
