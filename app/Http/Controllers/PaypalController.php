99d1b1bfae53e758504ffddb5701f75ecf9876fe
narita <238369+narita1980@users.noreply.github.com>
2019-05-15 08:59:27 +0900

    検索結果はまだ取得できないけど一旦コミットします

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -3,22 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PayPal\Api\Payment;
 use PayPal\Auth\OAuthTokenCredential;
 use PayPal\Rest\ApiContext;
 
 class PaypalController extends Controller
 {
-    private $api_context;
+    private $apiContext;
 
-    public function __construct() {
+    public function __construct()
+    {
         /** PayPal api context **/
-        $this->api_context = new ApiContext(
-            new OAuthTokenCredential(config('paypal.client_id'), config('paypal.secret'))
-        );
-        $this->api_context->setConfig(config('paypal.settings'));
+        $this->apiContext = $this->getPayPalApiContext(config('paypal.client_id'), config('paypal.secret'));
+        $this->apiContext->setConfig(config('paypal.settings'));
     }
 
-    public function index(Request $request){
-        return response()->json(['apple'=>'red','peach'=>'pink']);
+    public function index(Request $request)
+    {
+        try {
+            $params = array('count' => 10, 'start_index' => 5);
+            $payments = Payment::all($params, $this->apiContext);
+        } catch (Exception $ex) {
+        }
+
+//        return response()->json(['apple'=>'red','peach'=>'pink']);
+        return response()->json($payments);
+
+    }
+
+    /**
+     * Helper method for getting an APIContext for all calls
+     *
+     * @param string $clientId Client ID
+     * @param string $clientSecret Client Secret
+     * @return PayPal\Rest\ApiContext
+     */
+    function getPayPalApiContext($clientId, $clientSecret)
+    {
+        $apiContext = new ApiContext(
+            new OAuthTokenCredential(
+                $clientId,
+                $clientSecret
+            )
+        );
+
+        $apiContext->setConfig(
+            array(
+                'mode' => 'sandbox',
+                'log.LogEnabled' => true,
+                'log.FileName' => '../PayPal.log',
+                'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+                'cache.enabled' => true,
+            )
+        );
+
+        return $apiContext;
     }
 }
